856d4844ec005e8de6bd786831489b74830f98d1
ohi-noyi <iamjad96@gmail.com>
2025-04-14 17:34:43 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+
+
+Console.WriteLine("Enter number :");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int a = num1 % 2;
+
+
+if(a != 0){
+  Console.WriteLine($" {num1} is an odd number. ");  
+}
+else{
+     Console.WriteLine($" {num1} is an even number. ");
+}
